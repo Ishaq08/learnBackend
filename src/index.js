2fd3e607846776345d1775461f3da9c15333ea99
ishaq08 <Ishaq>
2025-05-18 00:25:02 +0500

    Connect to mongo altas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+//require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+/* // first apporch
+import express from 'express' // some time initalize app in index file
+const app = new express()
+ 
+(async() => {
+    try {
+         await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("Error", error)
+        throw error
+    }
+})()
+*/
