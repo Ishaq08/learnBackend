242fa7b420b70aeb8958beb1cee4ced23173840e
ishaq08 <Ishaq>
2025-05-23 02:44:50 +0500

    connect DB

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
-
-
-
-
-
+connectDB()
+.then((result) => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Sever is runing at Port: ${process.env.PORT}`);
+    })
+}).catch((err) => {
+    console.log("mongo db connection falied !!!", err);
+});
 
 
 /* // first apporch
