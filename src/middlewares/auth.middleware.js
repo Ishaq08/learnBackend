75dcb2fc65ba91d06a798e567211711ac6501d8e
ishaq08 <Ishaq>
2025-06-27 16:00:48 +0500

    refreshToke accessToken

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js"
+
+// when res can not be use write _
+export const verifyJwt = asyncHandler(async (req, _, next) => {
+    req.cookie?.accessToken || req.header
+        (" Authorization").replace("Bearer ", "")
+    
+try {
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request")
+        }// when token is not present
+    
+        //when token is present 
+       const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if (!user) {
+            // discuss about frontend
+            throw new ApiError( 401, "invalid Access Token")
+        }
+    
+        req.user = user;
+    next()
+    
+} catch (error) {
+    throw new ApiError(401, error?.massage || "invalid accesss token")
+}
+    
+})
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { regsiterUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, regsiterUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,5 +18,12 @@ router.route("/register").post(
     ]),
     regsiterUser
 )
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJwt, logoutUser)// verifyjwt is middleware , which can complete his work and move to next
+
+
 
 export default router
