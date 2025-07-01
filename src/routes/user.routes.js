c7d8b7148060028eaa46c64b5f17ca12cf983c99
ishaq08 <Ishaq>
2025-07-02 00:39:39 +0500

    management routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, regsiterUser } from "../controllers/user.controller.js";
+import { ChangeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, regsiterUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -23,7 +23,14 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser)// verifyjwt is middleware , which can complete his work and move to next
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, ChangeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/c/c:username").get(verifyJwt, getUserChannelProfile)
+router.route("/history").get(verifyJwt, getWatchHistory)
+
 
 export default router
